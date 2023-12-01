54966a0a7030c2a9bfb693a32d5b1061d41847af
jevans556 <47763236+jevans556@users.noreply.github.com>
2023-12-01 13:05:09 -0500

    Add Akil's changes

--- main.py
+++ main.py
@@ -6,7 +6,49 @@ import random
 
 app = Ursina()
 started = False
-EditorCamera()
+
+camera = EditorCamera(orthographic=True)
+camera.rotation_x = 90  # Set the rotation to achieve top-down view
+camera.position = (0, 30, 0)  # Adjust the height as needed
+
+terrain = Entity(model='plane', scale=(105, 1, 105), texture= 'textures/Grass2.jpg')
+
+# Make sure to set the model for lakes and forests
+big_lake = Entity(model='cube', scale=(30, 1, 30), position=(-20, 0, 29), texture='textures/water.jpeg')
+small_lake1 = Entity(model='cube', scale=(9, 1, 9), position=(-25, 0, -10), texture='textures/water.jpeg')
+small_lake2 = Entity(model='cube', scale=(11, 1, 11), position=(5, 0, -30), texture='textures/water.jpeg')
+small_lake3 = Entity(model='cube', scale=(14, 1, 9), position=(26, 0, 0), texture='textures/water.jpeg')
+
+dark_green = color.rgb(0, 0.5, 0)
+def create_forest(x1, x2, y1, y2):
+    x_pos = random.uniform(x1, x2)  # Adjust the x-axis range
+    z_pos = random.uniform(y1, y2)  # Adjust the z-axis range
+    return Entity(model='cube', scale=(6, 1, 6), position=(x_pos, 0, z_pos), texture='textures/bushtree.png')
+
+# Create a forest with a random cluster of trees
+
+#Forest 1
+for _ in range(17):
+    create_forest(17, 35, 24, 35)
+for _ in range(5):
+    create_forest(20, 35, 24, 30)
+
+#Forest 2
+for _ in range(6):
+    create_forest(-10, 0, 2, 8)
+
+#Forest 3
+for _ in range(8):
+    create_forest(-27, -40, -30, -35)
+
+#Forest 4
+for _ in range(14):
+    create_forest(20, 30, -20, -30)
+
+#Scatered trees
+for _ in range(6):
+    create_forest(-47, 47, -47, 47)
+
 bunny_population = []
 food_list = []
 predator_population=[]
